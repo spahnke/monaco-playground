8dccfa2a2d5415c6c2c3d059d83f18548cbfc21e
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2018-09-06 15:29:11 +0200

    Add a wrapper class for the editor

--- MonacoPlayground/wwwroot/app.ts
+++ MonacoPlayground/wwwroot/app.ts
@@ -1,32 +1,10 @@
-require.config({ paths: { vs: "monaco/min/vs" } });
-require(["vs/editor/editor.main"], function () {
-	// monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-	// 	target: monaco.languages.typescript.ScriptTarget.ES2017
-	// });
+import { CodeEditor } from "./code-editor.js";
 
-	const editor = monaco.editor.create(document.querySelector(".editor"), {
-		value: `function x() {
-    console.log('Hello world!');
-}`,
-		language: "javascript",
-		theme: "vs"
-	});
+async function main() {
+	const editor = await CodeEditor.create(document.querySelector(".editor"), "javascript");
+	editor.setContent(`function x() {
+	console.log('Hello world!');
+}`);
+}
 
-	const originalModel = monaco.editor.createModel("var foo = 'bar';", "javascript");
-	const modifiedModel = monaco.editor.createModel("var test = 42", "javascript");
-
-	const diffEditor = monaco.editor.createDiffEditor(document.querySelector(".diff"), {
-		automaticLayout: true,
-		contextmenu: true,
-		enableSplitViewResizing: false,
-		hover: { enabled: true },
-		ignoreTrimWhitespace: true,
-		originalEditable: true,
-		readOnly: false,
-		theme: "vs"
-	});
-	diffEditor.setModel({
-		original: originalModel,
-		modified: modifiedModel
-	});
-});
+main();
---
+++ MonacoPlayground/wwwroot/code-editor.ts
@@ -0,0 +1,46 @@
+﻿export class CodeEditor {
+    private editor: monaco.editor.IStandaloneCodeEditor;
+
+	static create(element: HTMLElement, language: string): Promise<CodeEditor> {
+		return new Promise(resolve => {
+			require.config({ paths: { vs: "monaco/min/vs" } });
+			require(["vs/editor/editor.main"], () => {
+				resolve(new CodeEditor(monaco.editor.create(element, {
+					language,
+					theme: "vs"
+				})));
+			});
+		});
+	}
+
+	private constructor(editor: monaco.editor.IStandaloneCodeEditor) {
+		this.editor = editor;
+	}
+
+	setContent(content: string) {
+		this.editor.setValue(content);
+	}
+}
+
+
+
+
+//-----------------------------------------------------------------------------------------
+// diff editor:
+//const originalModel = monaco.editor.createModel("var foo = 'bar';", "javascript");
+//const modifiedModel = monaco.editor.createModel("var test = 42", "javascript");
+
+//const diffEditor = monaco.editor.createDiffEditor(document.querySelector(".diff"), {
+//	automaticLayout: true,
+//	contextmenu: true,
+//	enableSplitViewResizing: false,
+//	hover: { enabled: true },
+//	ignoreTrimWhitespace: true,
+//	originalEditable: true,
+//	readOnly: false,
+//	theme: "vs"
+//});
+//diffEditor.setModel({
+//	original: originalModel,
+//	modified: modifiedModel
+//});
