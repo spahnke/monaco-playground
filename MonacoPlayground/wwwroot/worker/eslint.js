6bb0d162613a4e1f62b9b75237e1fcdcf9eaf851
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2018-09-08 20:16:57 +0200

    Inline call to ESLint

--- MonacoPlayground/wwwroot/worker/eslint.js
+++ MonacoPlayground/wwwroot/worker/eslint.js
@@ -1,13 +1,9 @@
 importScripts("/lib/eslint/eslint.js");
 var linter = new eslint();
 
-function lint(code, config) {
-    return linter.verify(code, config);
-}
-
 addEventListener("message", function(e) {
     postMessage({
         id: e.data.id,
-        messages: lint(e.data.code, e.data.config)
+        messages: linter.verify(e.data.code, e.data.config)
     });
 });
