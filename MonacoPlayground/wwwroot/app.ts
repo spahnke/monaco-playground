e2714b6823a6bd1c326c9dfb549054930e35b4e1
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2018-09-08 18:28:06 +0200

    Add a button to lint the contents of the editor

--- MonacoPlayground/wwwroot/app.ts
+++ MonacoPlayground/wwwroot/app.ts
@@ -1,4 +1,7 @@
 import { CodeEditor } from "./code-editor.js";
+import { Linter } from "./linter.js";
+
+const linter = new Linter();
 
 async function main() {
 	const editor = await CodeEditor.create(document.querySelector(".editor"));
@@ -15,6 +18,28 @@ async function main() {
 
 const foo = new Foo();
 foo.bar = Facts.next();`);
+	
+	const config = {
+		parserOptions: {
+			ecmaVersion: 2018,
+			ecmaFeatures: {
+				impliedStrict: true
+			},
+		},
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "error",
+			"semi": "warn"
+		},
+		globals: {
+			Facts: true,
+		}
+	};
+	
+	document.querySelector("#lint").addEventListener("click", async () => {
+		const result = await linter.lint(editor.getText(), config);
+		console.log(result);
+	});
 }
 
 main();
