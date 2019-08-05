373a08d416d3df9388bd05873db15a69a5f525fe
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2019-08-05 13:16:42 +0200

    Make sure to only register provider once

--- MonacoPlayground/wwwroot/src/languages/javascript/javascript-extensions.ts
+++ MonacoPlayground/wwwroot/src/languages/javascript/javascript-extensions.ts
@@ -1,12 +1,12 @@
 ﻿import { EsLintDiagnostics } from "./eslint-diagnostics.js";
 
-const alreadyRegistered = false;
+let alreadyRegistered = false;
 
 export async function registerJavascriptLanguageExtensions() {
 	if (alreadyRegistered) {
 		return;
 	}
-
 	const config = await fetch("eslintrc.json").then(r => r.json());
 	monaco.languages.registerCodeActionProvider("javascript", new EsLintDiagnostics(config));
+	alreadyRegistered = true;
 }
