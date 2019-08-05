7ec7d3d1cb0608883c00337f18206a7643775759
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2019-08-05 13:25:07 +0200

    Remove old linting technique

--- MonacoPlayground/wwwroot/src/app.ts
+++ MonacoPlayground/wwwroot/src/app.ts
@@ -1,5 +1,4 @@
 ﻿import { CodeEditor } from "./code-editor.js";
-import { XmlLint } from "./linter/xmllint.js";
 
 async function main() {
 	const editor = await CodeEditor.create(document.querySelector(".editor") as HTMLElement);
@@ -17,35 +16,6 @@ async function main() {
 
 const foo = new Foo();
 foo.bar = Facts.next();`, "javascript");
-
-
-//	editor.setContents(`<?xml version="1.0" encoding="UTF-8"?>
-
-//<shiporder orderid="889923"
-//xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-//xsi:noNamespaceSchemaLocation="shiporder.xsd">
-//  <orderperson>John Smith</orderperson>
-//  <shipto>
-//    <name>Ola Nordmann</name>
-//    <address>Langgt 23</address>
-//    <city>4000 Stavanger</city>
-//    <country>Norway</country>
-//  </shipto>
-//  <item>
-//    <title>Empire Burlesque</title>
-//    <note>Special Edition</note>
-//    <quantity>1</quantity>
-//    <price>10.90</price>
-//  </item>
-//  <item>
-//    <title>Hide your heart</title>
-//    <quantity>1</quantity>
-//    <price>9.90</price>
-//  </item>
-//</shiporder>`, "xml");
-
-//	const xsd = await fetch("example.xsd").then(r => r.text());
-//	editor.setLinter(new XmlLint(xsd, editor.editor));
 }
 
 main();
