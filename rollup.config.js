a98d9269a7b417603a820df73d47db40b971e0a9
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2019-11-12 13:59:43 +0100

    Combine configs

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,34 @@ import { terser } from "rollup-plugin-terser";
 
 const isProduction = process.env.ROLLUP_WATCH !== "true";
 
-export default {
-	input: "src/app.ts",
-	output: {
-		dir: "wwwroot/dist",
-		format: "es",
-		sourcemap: true,
+export default [
+	{
+		input: "src/app.ts",
+		output: {
+			dir: "wwwroot/dist",
+			format: "es",
+			sourcemap: true,
+		},
+		plugins: [
+			typescript({
+				tsconfig: "src/tsconfig.json",
+			}),
+			isProduction && terser()
+		],
 	},
-	plugins: [
-		typescript({
-			tsconfig: "src/tsconfig.json",
-		}),
-		isProduction && terser()
-	],
-}
+	{
+		input: "src/languages/javascript/worker/eslint-worker.ts",
+		output: {
+			dir: "wwwroot/worker",
+			format: "amd",
+			sourcemap: true,
+		},
+		external: ["/lib/eslint/eslint.js"],
+		plugins: [
+			typescript({
+				tsconfig: "src/tsconfig.worker.json",
+			}),
+			isProduction && terser()
+		],
+	}
+]
