0cf86d83dda82e961805dc74b845ca499a846f84
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2020-06-23 16:18:15 +0200

    More clarification

--- experiments/module.js
+++ experiments/module.js
@@ -1,4 +1,4 @@
-// dynamically generated -> can use a custom DiagnosticsAdapter to analyze models on changes (works transitively)
+// dynamically generated -> can use a custom DiagnosticsAdapter to analyze models on changes and generate these files (works transitively)
 // javascript text model for the editor -> defines the type of the variable(one for each module)
 __moduleDefinitions["asdf"] = defineModule("asdf", () => {
 	return class Foo {
