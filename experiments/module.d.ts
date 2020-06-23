6dd2ec055ab5f0f263403c35c222cd7582cf12b3
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2020-06-23 16:17:38 +0200

    Add more clarifying comments

--- experiments/module.d.ts
+++ experiments/module.d.ts
@@ -1,8 +1,9 @@
 // general definition file
 declare function defineModule<T>(name: string, definition: () => T): T;
 declare function requiredModule(name: string): any;
+
+// dynamically generated definition file
 /** Not a real object! Only relevant for module code completion. */
 namespace __moduleDefinitions { }
-
 // module definition file (all modules including name -> enables name completion)
 declare function requiredModule(name: "asdf"): typeof __moduleDefinitions["asdf"];
