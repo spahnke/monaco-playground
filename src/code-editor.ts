bd5650bc4fd333a84b6cae8eef6f1826ef5c77ac
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2020-10-06 12:07:41 +0200

    Add back lightbulb workaround as it is still needed

--- src/code-editor.ts
+++ src/code-editor.ts
@@ -76,6 +76,7 @@ export class CodeEditor {
 	setReadonly(value: boolean) {
 		this.editor.updateOptions({
 			readOnly: value,
+			lightbulb: { enabled: !value }, // because of https://github.com/microsoft/monaco-editor/issues/1596
 		});
 	}
 
