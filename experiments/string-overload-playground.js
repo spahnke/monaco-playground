0ba1752769eb264d11b253c342e7117dc9bccfda
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2020-10-07 10:08:37 +0200

    Remove bug example because this is fixed now

--- experiments/string-overload-playground.js
+++
@@ -1,9 +0,0 @@
-monaco.languages.typescript.javascriptDefaults.addExtraLib(`
-declare function foo(name: "Foo.Bar.Baz"): number
-declare function foo(name: "Foo.Bar.Qux"): string
-`, 'test.d.ts');
-
-monaco.editor.create(document.getElementById("container"), {
-	value: `foo('Foo.Bar.')`, // overload completion replaces everything in VS Code, but inserts at the end in Monaco -> why?
-	language: "javascript"
-});
