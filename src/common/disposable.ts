c746a973fa34c70ae323a9f083c57251cea6e41f
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2020-10-17 22:01:35 +0200

    Pass registered disposables through and be defensive when disposing

--- src/common/disposable.ts
+++ src/common/disposable.ts
@@ -4,14 +4,16 @@ export class Disposable implements monaco.IDisposable {
 	private disposables: monaco.IDisposable[] = [];
 
 	/**
-	 * Registers one ore more disposable objects that will be disposed when this instance is disposed.
+	 * Registers one ore more disposable objects that will be disposed when this instance is disposed
+	 * and returns the objects again.
 	 */
-	register(...disposables: monaco.IDisposable[]) {
+	register(...disposables: monaco.IDisposable[]): monaco.IDisposable[] {
 		this.disposables.push(...disposables);
+		return disposables;
 	}
 
 	dispose() {
 		for (const disposable of this.disposables)
-			disposable.dispose();
+			disposable?.dispose(); // be defensive here and use ?.
 	}
 }
