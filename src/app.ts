1aab5f4c73ef78fd265507dc1a19311ac5ba5ae6
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2021-01-24 09:06:05 +0100

    Inline constant

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,6 @@ import { PlaygroundContribution } from "./contrib/playground-contribution.js";
 import { TodoContribution } from "./contrib/todo-contribution.js";
 import { CodeEditor } from "./code-editor.js";
 
-const lib: ILibrary = {
-	contents: `
-declare class Facts {
-	/**
-	 * Returns the next fact
-	 *
-	 * [Online documentation](http://www.google.de)
-	 */
-	static next(): string;
-}`,
-	language: "typescript",
-	filePath: "test.d.ts"
-};
-
 async function main() {
 	const editor = await CodeEditor.create(document.querySelector<HTMLElement>(".editor")!);
 
@@ -26,7 +12,19 @@ async function main() {
 	const debug = editor.register(new DebugContribution(editor.editor));
 	debug.simulateDebugging();
 
-	editor.addLibrary(lib);
+	editor.addLibrary({
+		contents: `
+declare class Facts {
+	/**
+	 * Returns the next fact
+	 *
+	 * [Online documentation](http://www.google.de)
+	 */
+	static next(): string;
+}`,
+		language: "typescript",
+		filePath: "test.d.ts"
+	});
 
 	editor.setContents(`class Foo {
 	/**
