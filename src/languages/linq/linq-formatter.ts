e2e6c0a8037c0bdc6b3443391ca1439afe015723
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2021-07-06 12:19:41 +0200

    Use double quotes

--- src/languages/linq/linq-formatter.ts
+++ src/languages/linq/linq-formatter.ts
@@ -15,24 +15,24 @@
 	}
 
     public static removeLineBreaksAndExtraWhitespace(expression: string): string {
-        return expression.replace(/\r\n/g, ' ').replace(/\n/g, ' ').replace(/\t/g, ' ').replace(/\s{2,}/g, ' ');
+        return expression.replace(/\r\n/g, " ").replace(/\n/g, " ").replace(/\t/g, " ").replace(/\s{2,}/g, " ");
 	}
 
     private static insertLineBreaks(expression: string): string {
-        expression = LinqFormatter.replaceIfNotInString(expression, ` from `, '\nfrom ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` where `, '\nwhere ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` select `, '\nselect ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` group `, '\ngroup ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` orderby `, '\norderby ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` join `, '\njoin ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` let `, '\nlet ');
-        expression = LinqFormatter.replaceIfNotInString(expression, `&&`, '\n\t&& ');
-        expression = LinqFormatter.replaceIfNotInString(expression, `\\|\\|`, '\n\t|| ');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` *{ *`, ' {\n\t');
-        expression = LinqFormatter.replaceIfNotInString(expression, ` *} *`, '\n}');
+        expression = LinqFormatter.replaceIfNotInString(expression, ` from `, "\nfrom ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` where `, "\nwhere ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` select `, "\nselect ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` group `, "\ngroup ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` orderby `, "\norderby ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` join `, "\njoin ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` let `, "\nlet ");
+        expression = LinqFormatter.replaceIfNotInString(expression, `&&`, "\n\t&& ");
+        expression = LinqFormatter.replaceIfNotInString(expression, `\\|\\|`, "\n\t|| ");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` *{ *`, " {\n\t");
+        expression = LinqFormatter.replaceIfNotInString(expression, ` *} *`, "\n}");
         expression = LinqFormatter.replaceIfNotInString(expression, ` *, *`, ",\n\t");
         expression = LinqFormatter.replaceIfNotInString(expression, ` *\\*\\/ `, "*/\n\t");
-        return LinqFormatter.replaceIfNotInString(expression, ` {2,}`, ' ');
+        return LinqFormatter.replaceIfNotInString(expression, ` {2,}`, " ");
 	}
 
     private static replaceIfNotInString(expression: string, pattern: string, replacement: string): string {
