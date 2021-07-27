1674a2e02b994c9c273044f417478ddb3145a239
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2021-07-27 11:58:27 +0200

    Use normal functions as arrow functions are discouraged in mocha

--- test/common/disposable.test.ts
+++ test/common/disposable.test.ts
@@ -1,8 +1,8 @@
 import { strictEqual } from "assert";
 import { Disposable } from "../../src/common/disposable.js";
 
-describe("Disposable", () => {
-	it("should call dispose for all registered disposables", () => {
+describe("Disposable", function () {
+	it("should call dispose for all registered disposables", function () {
 		const disposable = new Disposable();
 		let disposed = 0;
 		disposable.register({
