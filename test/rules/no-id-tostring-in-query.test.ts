292f85a549118bfa8c6e64b157823cb523b280a3
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2021-07-27 15:03:41 +0200

    Add test cases

--- test/rules/no-id-tostring-in-query.test.ts
+++ test/rules/no-id-tostring-in-query.test.ts
@@ -7,22 +7,80 @@ const ruleTester = new RuleTester();
 ruleTester.run("no-id-tostring-in-query", rule, {
 	valid: [
 		{
-			code: `linq.execute("asdf");`
-		}
+			code: `linq.execute("asdf");`,
+		},
+		{
+			code: "linq.execute(`asdf`);",
+			parserOptions: { ecmaVersion: 2015 },
+		},
+		{
+			code: `linq.execute('a x.foo.toString() === "asdf"');`,
+		},
+		{
+			code: 'linq.execute(`a x.foo.toString() === "asdf"`);',
+			parserOptions: { ecmaVersion: 2015 },
+		},
 	],
 	invalid: [
+		// single quotes
 		{
-			code: `linq.execute('asdf.id.toString() === "asdf"');`,
-			errors: 1
-		}
+			code: `linq.execute('a x.id.toString() === "asdf"');`,
+			errors: 1,
+		},
+		{
+			code: `linq.execute('a x.Id.toString() === "asdf"');`,
+			errors: 1,
+		},
+		{
+			code: `linq.execute('a x.iD.toString() === "asdf"');`,
+			errors: 1,
+		},
+		{
+			code: `linq.execute('a x.ID.toString() === "asdf"');`,
+			errors: 1,
+		},
+		{
+			code: `linq.execute('a x.fooId.toString() === "asdf"');`,
+			errors: 1,
+		},
+		{
+			code: `linq.execute('a x.id.toString() === "asdf" asdf x.id.toString() === "qwer"');`,
+			errors: 2,
+		},
+		// template strings
+		{
+			code: 'linq.execute(`a x.id.toString() === "asdf" asdf`);',
+			errors: 1,
+			parserOptions: { ecmaVersion: 2015 },
+		},
+		{
+			code: 'linq.execute(`a x.id.toString() === "${text}" asdf`);',
+			errors: 1,
+			parserOptions: { ecmaVersion: 2015 },
+		},
+		// concatenated string
+		{
+			code: `linq.execute(foo + 'a x.id.toString() === "' + foo + '" asdf ' + \`a x.id.toString() !== "\${text}" asdf \` + foo);`,
+			errors: 2,
+			parserOptions: { ecmaVersion: 2015 },
+		},
+		// intermediate variable
+		{
+			code: `
+const query = foo + 'a x.id.toString() === "' + foo + \`a x.id.toString() !== "\${text}" asdf\` + foo;
+linq.execute(query);
+`.trim(),
+			errors: 2,
+			parserOptions: { ecmaVersion: 2015 },
+		},
+		{
+			code: `
+const query = foo + 'a x.id.toString() === "' + foo + \`a x.id.toString() !== "\${text}" asdf\` + foo;
+const query2 = query;
+linq.execute(query2);
+`.trim(),
+			errors: 2,
+			parserOptions: { ecmaVersion: 2015 },
+		},
 	]
 });
-
-// const query = foo + 'a x.id.toString() === "' + foo + `a x.id.toString() !== "${text}" asdf` + foo;
-// const query2 = query;
-// linq.execute('a x.id.toString() === "asdf" asdf x.id.toString() === "qwer"');
-// linq.execute(`a x.id.toString() === "asdf" asdf`);
-// linq.execute(`a x.id.toString() === "${text}" asdf`);
-// linq.execute(foo + 'a x.id.toString() === "' + foo + '" asdf ' + `a x.id.toString() !== "${text}" asdf ` + foo);
-// linq.execute(query);
-// linq.execute(query2);
