fd71e804c40168db170c0c791d7246edd8175ac9
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2026-01-21 11:04:13 +0100

    Add cursor state handling to be able to show line/column info in addition to how much is selected; add more helper methods

--- src/code-editor.ts
+++ src/code-editor.ts
@@ -1,8 +1,18 @@
 import { Disposable } from "./common/disposable.js";
-import { addLibrary, delay, ILibrary } from "./common/monaco-utils.js";
+import { addLibrary, addTemplates, delay, ILibrary, ITemplate } from "./common/monaco-utils.js";
 import { LocalStorageEditorConfiguration } from "./editor-config.js";
 
+export interface CursorState {
+	position: monaco.IPosition;
+	selectedCharacters: number;
+	selectedLines: number;
+	selections: number;
+}
+
 export class CodeEditor extends Disposable {
+	private cursorState: CursorState = { position: { lineNumber: 1, column: 1 }, selectedCharacters: 0, selectedLines: 0, selections: 0 };
+	private readonly onDidCursorStateChangeEmitter = this.register(new monaco.Emitter<CursorState>());
+
 	static create(element: HTMLElement, language?: string): CodeEditor {
 		return new CodeEditor(monaco.editor.create(element, {
 			automaticLayout: true,
@@ -55,9 +65,35 @@ export class CodeEditor extends Disposable {
 
 	private constructor(public readonly editor: monaco.editor.IStandaloneCodeEditor) {
 		super();
+		this.register(this.editor.onDidChangeCursorPosition(e => {
+			this.cursorState.position = e.position;
+			this.onDidCursorStateChangeEmitter.fire(this.cursorState);
+		}));
+		this.register(this.editor.onDidChangeCursorSelection(e => {
+			this.cursorState.selectedLines = 0;
+			this.cursorState.selectedCharacters = 0;
+			this.cursorState.selections = 1 + e.secondarySelections.length;
+			for (const selection of [e.selection, ...e.secondarySelections]) {
+				if (!monaco.Selection.isEmpty(selection)) {
+					this.cursorState.selectedLines += selection.endLineNumber - selection.startLineNumber + 1;
+					let selectedCharacters = selection.endColumn - selection.startColumn;
+					if (monaco.Selection.spansMultipleLines(selection)) {
+						const model = this.editor.getModel();
+						if (model) {
+							selectedCharacters = model.getOffsetAt(selection.getEndPosition()) - model.getOffsetAt(selection.getStartPosition());
+						}
+					}
+					this.cursorState.selectedCharacters += selectedCharacters;
+				}
+			}
+			this.onDidCursorStateChangeEmitter.fire(this.cursorState);
+		}));
 		this.register(new LocalStorageEditorConfiguration(editor));
 	}
 
+	readonly onDidCursorStateChange = this.onDidCursorStateChangeEmitter.event;
+
+	/** Replaces the entire editor model, allowing a change of language and content. Does NOT preserve the undo stack! */
 	setContents(content: string, language?: string, fileName?: string): void {
 		this.editor.getModel()?.dispose();
 		const uri = fileName !== undefined ? monaco.Uri.file(fileName) : undefined;
@@ -85,8 +121,17 @@ export class CodeEditor extends Disposable {
 		return this.editor.getValue();
 	}
 
+	/** Replaces the text of the current model (if there is one). Does NOT preserve the undo stack! */
+	setText(text: string): void {
+		this.editor.getModel()?.setValue(text);
+	}
+
 	focus(): void {
 		this.editor.focus();
+	}
+
+	refresh(): void {
+		this.editor.layout();
 	}
 
 	setReadonly(value: boolean): void {
@@ -139,6 +184,11 @@ export class CodeEditor extends Disposable {
 		this.editor.revealPositionInCenterIfOutsideViewport(position);
 	}
 
+	zoom(zoomFactor: number): void {
+		const zoomLevel = Math.round((zoomFactor - 1) / 0.1);
+		monaco.editor.EditorZoom.setZoomLevel(zoomLevel);
+	}
+
 	zoomIn(): void {
 		this.editor.trigger("zoom", "editor.action.fontZoomIn", null);
 	}
@@ -177,6 +227,10 @@ export class CodeEditor extends Disposable {
 		return false;
 	}
 
+	getCursorState(): CursorState {
+		return this.cursorState;
+	}
+
 	getCurrentWord(): string | undefined {
 		const position = this.editor.getPosition();
 		if (position === null)
@@ -184,12 +238,32 @@ export class CodeEditor extends Disposable {
 		return this.editor.getModel()?.getWordAtPosition(position)?.word;
 	}
 
-	addChangeListener(listener: () => void): void {
-		this.editor.onDidChangeModelContent(e => listener());
+	getPreviousWord(): string | undefined {
+		const currentPosition = this.editor.getPosition();
+		if (currentPosition == null)
+			return undefined;
+
+		const currentWord = this.editor.getModel()?.getWordAtPosition(currentPosition);
+		if (!currentWord || currentWord.startColumn === 0)
+			return undefined;
+
+		const word = this.editor.getModel()?.getWordAtPosition({ lineNumber: currentPosition.lineNumber, column: currentWord.startColumn - 1 });
+		return !word ? undefined : word.word;
+	}
+
+	addChangeListener(listener: () => void): monaco.IDisposable {
+		return this.editor.onDidChangeModelContent(e => listener());
 	}
 
 	addLibrary(library: ILibrary): void {
 		this.register(addLibrary(library));
+	}
+
+	addTemplates(templates: ITemplate[]): void {
+		const model = this.editor.getModel();
+		if (model === null)
+			return;
+		this.register(addTemplates(model.getLanguageId(), templates));
 	}
 
 	override dispose(): void {
