4958807e7137285291eef0529f47559b323778e4
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2026-07-21 11:08:56 +0200

    Add debug overlay and action placeholders

--- src/contrib/debug-contribution.ts
+++ src/contrib/debug-contribution.ts
@@ -7,17 +7,57 @@ const contextMenuGroupId = "8_debug";
  * Adds debug UI capabilities to the editor.
  */
 export class DebugContribution extends Disposable {
-	private breakpointPreviewDecorations: monaco.editor.IEditorDecorationsCollection;
-	private breakpointDecorations: Map<string, monaco.editor.IModelDecoration> = new Map();
-	private currentDebugLineDecorations: monaco.editor.IEditorDecorationsCollection;
+	private readonly breakpointPreviewDecorations: monaco.editor.IEditorDecorationsCollection;
+	private readonly breakpointDecorations: Map<string, monaco.editor.IModelDecoration> = new Map();
+	private readonly currentDebugLineDecorations: monaco.editor.IEditorDecorationsCollection;
+	private readonly debugActiveContextKey: monaco.editor.IContextKey<boolean>;
 
 	constructor(private editor: monaco.editor.IStandaloneCodeEditor) {
 		super();
 		this.breakpointPreviewDecorations = editor.createDecorationsCollection();
 		this.currentDebugLineDecorations = editor.createDecorationsCollection();
 		this.enableGlyphMargin();
+		this.debugActiveContextKey = editor.createContextKey("debuggerSessionActive", false);
 		this.register(this.editor.onMouseMove(this.onMouseMove));
 		this.register(this.editor.onMouseDown(this.onMouseDown));
+		this.register(editor.addAction({
+			id: "debugger_start_session",
+			label: "Start Debugging",
+			keybindings: [monaco.KeyCode.F5],
+			precondition: "!debuggerSessionActive",
+			run: async () => {
+				console.log("Start debugging");
+				this.debugActiveContextKey.set(true);
+			},
+		}));
+		this.register(editor.addAction({
+			id: "debugger_continue",
+			label: "Start Debugging",
+			keybindings: [monaco.KeyCode.F5],
+			precondition: "debuggerSessionActive",
+			run: async () => {
+				console.log("Continue");
+			},
+		}));
+		this.register(editor.addAction({
+			id: "debugger_step_over",
+			label: "Start Debugging",
+			keybindings: [monaco.KeyCode.F10],
+			precondition: "debuggerSessionActive",
+			run: async () => {
+				console.log("Step over");
+			},
+		}));
+		this.register(editor.addAction({
+			id: "debugger_stop_session",
+			label: "Start Debugging",
+			keybindings: [monaco.KeyMod.Shift | monaco.KeyCode.F5],
+			precondition: "debuggerSessionActive",
+			run: async () => {
+				console.log("Stop debugging");
+				this.debugActiveContextKey.set(false);
+			},
+		}));
 		this.register(editor.addAction({
 			id: "toggle_breakpoint",
 			label: "Toggle Breakpoint",
@@ -27,41 +67,48 @@ export class DebugContribution extends Disposable {
 			run: () => this.toggleBreakpoint(),
 		}));
 
-		let currentLine = 1;
-		this.register(this.editor.addAction({
-			id: "debug_step",
-			label: "Step",
-			keybindings: [monaco.KeyCode.F10],
-			contextMenuGroupId,
-			contextMenuOrder: 1,
-			run: (editor) => {
-				const model = editor.getModel();
-				if (!model)
-					return;
-
-				this.displayCurrentlyDebuggedLine({
-					startLineNumber: currentLine,
-					endLineNumber: currentLine,
-					startColumn: model.getLineFirstNonWhitespaceColumn(currentLine),
-					endColumn: model.getLineLastNonWhitespaceColumn(currentLine)
-				});
-
-				currentLine++;
-				if (currentLine > model.getLineCount())
-					currentLine = 1;
+		const debugOverlay: monaco.editor.IOverlayWidget = {
+			getId() {
+				return "debugger_overlay_widget";
 			},
-		}));
-		this.register(this.editor.addAction({
-			id: "debug_stop",
-			label: "Reset Debugger",
-			keybindings: [monaco.KeyMod.Shift | monaco.KeyCode.F10],
-			contextMenuGroupId,
-			contextMenuOrder: 2,
-			run: () => {
-				this.removeDebugLine();
-				currentLine = 1;
+
+			getDomNode() {
+				const debugWidget = document.createElement("div");
+				debugWidget.className = "monaco-editor editor-widget find-widget visible";
+				debugWidget.style.display = "flex";
+				debugWidget.style.alignItems = "center";
+				const startAndContinueButton = document.createElement("div");
+				startAndContinueButton.className = "button codicon codicon-debug-start";
+				debugWidget.appendChild(startAndContinueButton);
+				const continueButton = document.createElement("div");
+				continueButton.className = "button codicon codicon-debug-continue";
+				debugWidget.appendChild(continueButton);
+				const pauseButton = document.createElement("div");
+				pauseButton.className = "button codicon codicon-debug-pause";
+				debugWidget.appendChild(pauseButton);
+				const stepOverButton = document.createElement("div");
+				stepOverButton.className = "button codicon codicon-debug-step-over";
+				debugWidget.appendChild(stepOverButton);
+				const stepIntoButton = document.createElement("div");
+				stepIntoButton.className = "button codicon codicon-debug-step-into";
+				debugWidget.appendChild(stepIntoButton);
+				const stepOutButton = document.createElement("div");
+				stepOutButton.className = "button codicon codicon-debug-step-out";
+				debugWidget.appendChild(stepOutButton);
+				const stopButton = document.createElement("div");
+				stopButton.className = "button codicon codicon-debug-stop";
+				debugWidget.appendChild(stopButton);
+				return debugWidget;
 			},
-		}));
+
+			getPosition() {
+				return {
+					preference: monaco.editor.OverlayWidgetPositionPreference.TOP_CENTER,
+				};
+			},
+		};
+		editor.addOverlayWidget(debugOverlay);
+		this.register(toDisposable(() => editor.removeOverlayWidget(debugOverlay)));
 	}
 
 	override dispose(): void {
