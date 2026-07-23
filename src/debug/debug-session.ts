c22faf2468d4c6f4cb326f17e8a40b045ac1fcbc
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2026-07-23 09:58:43 +0200

    Add state for pause information and all parsed scripts; add models with source text for the current call frame

--- src/debug/debug-session.ts
+++ src/debug/debug-session.ts
@@ -1,5 +1,6 @@
 import { Disposable } from "../common/disposable.js";
 import { DebugProtocol, Transport } from "./debug-protocol.js";
+import { type Protocol } from "./protocol";
 
 export class DebugSession extends Disposable {
 	private protocol?: DebugProtocol;
@@ -8,6 +9,26 @@ export class DebugSession extends Disposable {
 
 	readonly onDidChangeActiveState = this.activeEvent.event;
 	readonly onDidChangePausedState = this.pausedEvent.event;
+
+	// TODO(seb) Temp shape of state accessible during pause states. Consolidate what we actually need for UI and look
+	// into type/write safety.
+	readonly scriptMetadata = new Map<string, Protocol.Debugger.ScriptParsedEvent>();
+	readonly scriptModels = new Map<string, monaco.editor.ITextModel>();
+	readonly urlToScriptId = new Map<string, string>();
+	executionContext?: Protocol.Runtime.ExecutionContextCreatedEvent;
+	pauseState?: Protocol.Debugger.PausedEvent;
+
+	private resetState(): void {
+		this.protocol = undefined;
+		this.scriptMetadata.clear();
+		for (const model of this.scriptModels.values()) {
+			model.dispose();
+		}
+		this.scriptModels.clear();
+		this.urlToScriptId.clear();
+		this.executionContext = undefined;
+		this.pauseState = undefined;
+	}
 
 	async connect(transport: Transport): Promise<void> {
 		this.activeEvent.fire(true);
@@ -19,77 +40,51 @@ export class DebugSession extends Disposable {
 				console.error("Transport connection was closed unexpectedly", error);
 			}
 			this.activeEvent.fire(false);
-			this.protocol = undefined;
+			this.resetState();
 		});
 		this.protocol.runtime.on("executionContextCreated", params => {
-			console.log("Debugging session started", params);
+			console.log("Debugging session started");
+			this.executionContext = params;
 		});
-		this.protocol.runtime.on("executionContextDestroyed", params => {
-			console.log("Debugging session finished", params);
+		this.protocol.runtime.on("executionContextDestroyed", () => {
+			console.log("Debugging session finished");
+			this.executionContext = undefined;
 			this.disconnect();
 		});
-		this.protocol.runtime.on("consoleAPICalled", params => {
-			const args = params.args.map(arg => {
-				switch (arg.type) {
-					case "bigint": return BigInt(arg.unserializableValue!.slice(0, -1));
-					case "function": return arg.description ?? "<function>";
-					case "object": {
-						if (arg.subtype === "null") {
-							return null;
-						} else if (arg.preview) {
-							if (arg.subtype) {
-								return arg.description;
-							} else {
-								const obj = Object.create(null);
-								for (const prop of arg.preview.properties) {
-									let value;
-									switch (prop.type) {
-										case "bigint": value = BigInt(prop.value!.slice(0, -1)); break;
-										case "boolean": value = Boolean(prop.value); break;
-										case "number": value = Number(prop.value); break;
-										default: value = prop.value; break;
-									}
-									obj[prop.name] = value;
-								}
-								return obj;
-							}
-						} else {
-							return "<object>";
-						}
-					}
-					case "symbol": return arg.description ?? "<symbol>";
-					default: return arg.value;
-				}
-			});
-			switch (params.type) {
-				case "assert": console.assert(...args); break;
-				case "warning": console.warn(...args); break;
-				case "startGroup": console.group(...args); break;
-				case "startGroupCollapsed": console.groupCollapsed(...args); break;
-				case "endGroup": console.groupEnd(); break;
-				case "profile": console.time(...args); break;
-				case "profileEnd": console.timeEnd(...args); break;
-				default: console[params.type](...args); break;
+		this.protocol.runtime.on("consoleAPICalled", DebugSession.handleConsoleApiCall);
+		this.protocol.debugger.on("scriptParsed", params => {
+			this.scriptMetadata.set(params.scriptId, params);
+			if (params.url) {
+				this.urlToScriptId.set(params.url, params.scriptId);
 			}
 		});
-		this.protocol.debugger.on("scriptParsed", async params => {
-			console.log("scriptParsed notification", params);
-			const source = await this.protocol?.debugger.getScriptSource({ scriptId: params.scriptId });
-			console.log("script src result", source);
-		});
-		this.protocol.debugger.on("paused", params => {
-			console.log("paused notification", params);
+		this.protocol.debugger.on("paused", async (params) => {
+			this.pauseState = params;
+			const scriptId = params.callFrames[0].location.scriptId;
+			const metadata = this.scriptMetadata.get(scriptId);
+			if (metadata && !this.scriptModels.has(scriptId)) {
+				if (metadata.scriptLanguage === "JavaScript") {
+					const scriptSource = await this.protocol!.debugger.getScriptSource({ scriptId });
+					const model = monaco.editor.createModel(scriptSource.scriptSource, metadata.scriptLanguage, metadata.url ? monaco.Uri.parse(metadata.url) : undefined);
+					this.scriptModels.set(scriptId, model);
+				} else if (metadata.scriptLanguage === "WebAssembly") {
+					const wasmModule = await this.protocol!.debugger.disassembleWasmModule({ scriptId });
+					const wat = wasmModule.chunk.lines.join("\n");
+					// TODO(seb) Add syntax definition for WAT
+					const model = monaco.editor.createModel(wat, metadata.scriptLanguage, metadata.url ? monaco.Uri.parse(metadata.url) : undefined);
+					this.scriptModels.set(scriptId, model);
+				}
+			}
 			this.pausedEvent.fire(true);
 		});
 		this.protocol.debugger.on("resumed", () => {
-			console.log("resumed notification");
+			// TODO(seb) Do we want to clear this or leave it accessible to the UI for rendering?
+			this.pauseState = undefined;
 			this.pausedEvent.fire(false);
 		});
 		if (await this.protocol.transport.connect()) {
 			await this.protocol.runtime.enable();
-			console.log("Runtime.enable done");
-			const enableResult = await this.protocol.debugger.enable({});
-			console.log("Debugger.enable response", enableResult);
+			await this.protocol.debugger.enable({});
 			await this.protocol.runtime.runIfWaitingForDebugger();
 		}
 	}
@@ -120,11 +115,56 @@ export class DebugSession extends Disposable {
 
 	disconnect(): void {
 		this.protocol?.transport.disconnect();
-		this.protocol = undefined;
 	}
 
 	override dispose(): void {
 		this.disconnect();
+		this.resetState();
 		super.dispose();
+	}
+
+	private static handleConsoleApiCall(params: Protocol.Runtime.ConsoleAPICalledEvent): void {
+		const args = params.args.map(arg => {
+			switch (arg.type) {
+				case "bigint": return BigInt(arg.unserializableValue!.slice(0, -1));
+				case "function": return arg.description ?? "<function>";
+				case "object": {
+					if (arg.subtype === "null") {
+						return null;
+					} else if (arg.preview) {
+						if (arg.subtype) {
+							return arg.description;
+						} else {
+							const obj = Object.create(null);
+							for (const prop of arg.preview.properties) {
+								let value;
+								switch (prop.type) {
+									case "bigint": value = BigInt(prop.value!.slice(0, -1)); break;
+									case "boolean": value = Boolean(prop.value); break;
+									case "number": value = Number(prop.value); break;
+									default: value = prop.value; break;
+								}
+								obj[prop.name] = value;
+							}
+							return obj;
+						}
+					} else {
+						return "<object>";
+					}
+				}
+				case "symbol": return arg.description ?? "<symbol>";
+				default: return arg.value;
+			}
+		});
+		switch (params.type) {
+			case "assert": console.assert(...args); break;
+			case "warning": console.warn(...args); break;
+			case "startGroup": console.group(...args); break;
+			case "startGroupCollapsed": console.groupCollapsed(...args); break;
+			case "endGroup": console.groupEnd(); break;
+			case "profile": console.time(...args); break;
+			case "profileEnd": console.timeEnd(...args); break;
+			default: console[params.type](...args); break;
+		}
 	}
 }
